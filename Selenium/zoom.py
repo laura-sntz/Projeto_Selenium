976229e8148e6e117c9349d93606f70f6ad79c6b
laura-stov <160499068+laura-stov@users.noreply.github.com>
2024-09-18 09:05:08 -0300

    Update zoom.py

--- Selenium/zoom.py
+++ Selenium/zoom.py
@@ -23,6 +23,8 @@ def adicionarProduto(lista):
 driver = webdriver.Edge()
 driver.get('https://www.zoom.com.br/')
 
+#concordando com os termos do site
+sleep(3)
 popup = driver.find_element(By.CLASS_NAME, 'PrivacyPolicy_Button__1RxwB')
 popup.click()
 
@@ -31,13 +33,15 @@ pesquisa = driver.find_element(By.CLASS_NAME, 'AutoCompleteStyle_autocomplete__B
 digitar = pesquisa.find_element(By.TAG_NAME, 'input')
 digitar.send_keys('celular', Keys.ENTER)
 
+#lista dos recomendados na página 1
 sleep(3)
 lista1 = []
 adicionarProduto(lista1)
 
 sleep(3)
 prox_pagina = driver.find_element(By.CLASS_NAME, 'Paginator_fullPage__YLBmh')
-botao = prox_pagina.find_element(By.TAG_NAME, 'a')
-botao.click()
+botao = prox_pagina.find_element(By.XPATH, '//a[@aria-label="Página 2"]')
+#tive que fazer por meio do javascript, pois vários métodos não funcionaram para fazer o botão ser clicado
+driver.execute_script("arguments[0].click();", botao)
 
 driver.close()
